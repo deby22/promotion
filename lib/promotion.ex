be9515c6b404c5568b4ef11548970297c095de73
ddeby <you@example.com>
2021-08-16 18:50:53 +0200

    refactor

--- lib/promotion.ex
+++ lib/promotion.ex
@@ -1,8 +1,20 @@
+# defmodule Promotion do
+#   def discount([]), do: 0
+#   def discount([_]), do: 0
+#   def discount(products) when length(products) == 2, do: 1
+#   def discount(products) when length(products) <= 5, do: 2
+#   def discount(products) when length(products) <= 10, do: 3
+#   def discount(_), do: 5
+# end
+
 defmodule Promotion do
-  def discount([]), do: 0
-  def discount([_]), do: 0
-  def discount(products) when length(products) == 2, do: 1
-  def discount(products) when length(products) <= 5, do: 2
-  def discount(products) when length(products) <= 10, do: 3
-  def discount(_), do: 5
+  def discount(products) do
+    cond do
+      length(products) > 10 -> 5
+      length(products) > 5 -> 3
+      length(products) > 2 -> 2
+      length(products) == 2 -> 1
+      true -> 0
+    end
+  end
 end
